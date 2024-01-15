da1b1af27a0d3d6891077bb934629a416de9091d
Jacob C. Hart <37990467+drdauntless@users.noreply.github.com>
2024-01-15 12:53:24 -0600

    update

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -3,8 +3,6 @@ def right_arrow():
     head_char = input()
 
     row1 = '      ' + head_char
-    row2 = base_char+base_char+base_char+base_char+base_char+base_char+head_char+head_char
-    row3 = base_char+base_char+base_char+base_char+base_char+base_char+head_char+head_char+head_char
     ''' Type your code here. '''
 
     print(row1)
