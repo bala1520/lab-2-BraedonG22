ad148bb88771e9b80c364bc6d47209c1a9685a06
BraedonG22 <bjg8631@utulsa.edu>
2024-01-31 14:31:23 -0600

    Made arrows

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -1,9 +1,16 @@
-def right_arrow():
-    base_char = input()
-    head_char = input()
+'''
+Name: Braedon Gehring
+Lab Time: Friday, 3:00
+'''
 
-    row1 = '      ' + head_char
-    ''' Type your code here. '''
+def right_arrow():
+    base_char = input("Character for arrow base")
+    head_char = input("Character for arrow head")
+
+    row1 = base_char + head_char
+    row2 = base_char + base_char + head_char
+    row3 = base_char + base_char + base_char + head_char
+
 
     print(row1)
     print(row2)
