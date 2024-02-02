9befb2b7d905c9657a6c5b4ce886f16c9fa95442
BraedonG22 <bjg8631@utulsa.edu>
2024-02-02 17:00:24 -0600

    Housing prices

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -1,8 +1,11 @@
 
 def real_estate():
-    
-    current_price = int(input())
-    last_month_price = int(input())
+    current_price = int(input('Current price of house: '))
+    last_month_price = int(input('Price of house last month: '))
     # Your code goes here
+    changeval = (current_price - last_month_price)
+    mortgage = (current_price * 0.051) / 12
+    print('This house is $' f'{current_price:.2f}' + '.' + ' ' + 'The change is $' f'{changeval:.2f}' + ' ' + 'since last month.')
+    print('The estimated monthly mortgae is $' f'{mortgage:.2f}' + '.')
 if __name__ == "__main__":
     real_estate()
