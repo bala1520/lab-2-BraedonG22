c007f8552c77bfd7a97ef641f16195ee07b13f07
BraedonG22 <bjg8631@utulsa.edu>
2024-02-02 16:38:17 -0600

    did Lab

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,10 +1,10 @@
 def telephone():
     phone_number = int(input('Write phone number (10-digits, no spaces): '))
     ''' Type your code here. '''
-    area_code = ([phone_number // 10000000])
+    area_code = (phone_number // 10000000)
     prefix = (phone_number % 10000000 // 10000)
     line_number = (phone_number % 10000)*-1
     
-    print(area_code,prefix,line_number)
+    print('(' + str(area_code) + ')' + " " + str(prefix) + str(line_number))
 if __name__ == "__main__":
     telephone()
