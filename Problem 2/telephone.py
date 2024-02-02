8cac068867a63e93c8d4966c33e020619ab940d9
BraedonG22 <bjg8631@utulsa.edu>
2024-02-02 15:30:04 -0600

    Couldn't get parentheses or remove the space, so I settled for brackets

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,10 +1,10 @@
 def telephone():
     phone_number = int(input('Write phone number (10-digits, no spaces): '))
     ''' Type your code here. '''
-    part1 = phone_number // 10000000
-    part2 = phone_number % 10000000 // 10000
-    part3 = phone_number % 10000
-
-    print(part1,part2,-part3) 
+    area_code = ([phone_number // 10000000])
+    prefix = (phone_number % 10000000 // 10000)
+    line_number = (phone_number % 10000)*-1
+    
+    print(area_code,prefix,line_number)
 if __name__ == "__main__":
     telephone()
